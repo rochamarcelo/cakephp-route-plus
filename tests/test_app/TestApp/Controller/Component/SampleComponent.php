fa22f2e195e93318c33f62c05bc849cbe713f7ca
Marcelo Rocha <contato@omarcelo.com.br>
2023-12-08 08:24:51 -0300

    Preparing controllers for test_app

---
+++ tests/test_app/TestApp/Controller/Component/SampleComponent.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace TestApp\Controller\Component;
+
+use Cake\Controller\Component;
+
+/**
+ * Sample component
+ */
+class SampleComponent extends Component
+{
+    /**
+     * Default configuration.
+     *
+     * @var array<string, mixed>
+     */
+    protected array $_defaultConfig = [];
+}
